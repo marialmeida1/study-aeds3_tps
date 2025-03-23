9ae502473ea6022f48df548682ea4f0343f08056
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-03-22 23:49:55 -0300

    feat: iniciando visao

---
+++ tp01/src/view/ViewEpisodio.java
@@ -0,0 +1,119 @@
+package tp01.src.view;
+
+import tp01.src.models.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ViewEpisodio {
+    private static final Scanner console = new Scanner(System.in);
+
+    public void exibirMenu() {
+        System.out.println("\n\nPUCFlix 1.0");
+        System.out.println("-------");
+        System.out.println("> Início > Episódio");
+        System.out.println("\n1 - Incluir");
+        System.out.println("2 - Buscar");
+        System.out.println("3 - Alterar");
+        System.out.println("4 - Excluir");
+        System.out.println("0 - Retornar ao menu anterior");
+        System.out.print("\nOpção: ");
+    }
+
+    public void mostraSerie(Episodio episodio) {
+        if (episodio != null) {
+            System.out.println("\nDetalhes do episodio:");
+            System.out.println("----------------------");
+            System.out.printf("Nome:................ %s%n", episodio.nome);
+            System.out.printf("Temporada:........... %d%n", episodio.temporada);
+            System.out.printf("Data Lançamento:..... %s%n",
+                    episodio.lancamento.format(DateTimeFormatter.ofPattern("yyyy")));
+            System.out.printf("Duração:............. %d%n", episodio.duracao);
+            System.out.println("----------------------");
+        } else {
+            System.out.println("Episódio não encontrado.");
+        }
+    }
+
+    public String obterNome() {
+        String nome;
+        while (true) {
+            System.out.print("\nNome (min. de 4 letras ou vazio para cancelar): ");
+            nome = console.nextLine();
+            if (nome.isEmpty()) {
+                return null; // Cancela a operação
+            }
+            if (nome.length() >= 4) {
+                return nome;
+            } else {
+                System.out.println("Nome inválido! O nome deve ter pelo menos 4 letras.");
+            }
+        }
+    }
+
+    public float obterDuracao() {
+        float duracao;
+        while (true) {
+            System.out.print("Salário: ");
+            if (console.hasNextInt()) {
+                duracao = console.nextInt();
+                if (duracao > 0) {
+                    console.nextLine();
+                    return duracao;
+                } else {
+                    System.out.println("Duração inválida! O valor deve ser maior que zero.");
+                }
+            } else {
+                System.out.println("Entrada inválida! Digite um valor numérico para a duração.");
+                console.nextLine(); 
+            }
+        }
+    }
+
+    public LocalDate obterDataLancamento() {
+        LocalDate dataLancamento;
+        while (true) {
+            System.out.print("Data de lançamento (DD/MM/AAAA): ");
+            String data = console.nextLine();
+            try {
+                dataLancamento = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                return dataLancamento;
+            } catch (Exception e) {
+                System.out.println("Data inválida! O formato correto é DD/MM/AAAA.");
+            }
+        }
+    }
+
+    public boolean confirmarAlteracoes() {
+        String resposta;
+        while (true) {
+            System.out.print("\nConfirma as alterações? (S/N) ");
+            resposta = console.nextLine().trim(); 
+
+            if (resposta.equalsIgnoreCase("S")) {
+                return true;
+            } else if (resposta.equalsIgnoreCase("N")) {
+                return false;
+            } else {
+                System.out.println("Resposta inválida! Por favor, digite 'S' para sim ou 'N' para não.");
+            }
+        }
+    }
+
+    public boolean confirmarExclusao() {
+        String resposta;
+        while (true) {
+            System.out.print("\nConfirma a exclusão do episódio? (S/N) ");
+            resposta = console.nextLine().trim(); 
+
+            if (resposta.equalsIgnoreCase("S")) {
+                return true;
+            } else if (resposta.equalsIgnoreCase("N")) {
+                return false;
+            } else {
+                System.out.println("Resposta inválida! Por favor, digite 'S' para sim ou 'N' para não.");
+            }
+        }
+    }
+}
