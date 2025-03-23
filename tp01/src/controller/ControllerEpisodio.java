e98663859b5f9b19da1e2d936f47bb274728659b
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-03-22 23:55:46 -0300

    feat: iniciando o arquivo de episodio

---
+++ tp01/src/controller/ControllerEpisodio.java
@@ -0,0 +1,137 @@
+package tp01.src.controller;
+
+import tp01.src.models.Cliente;
+import tp01.src.data.ArquivoCliente;
+import tp01.src.view.ViewSerie;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class ControllerEpisodio {
+
+    private ArquivoCliente arqClientes;
+    private ViewSerie visao;
+    private static final Scanner console = new Scanner(System.in);
+
+    public ControllerEpisodio() throws Exception {
+        arqClientes = new ArquivoCliente();
+        visao = new ViewSerie();
+    }
+
+    public void menu() throws Exception {
+        int opcao;
+        do {
+            visao.exibirMenu();
+            opcao = Integer.valueOf(console.nextLine());
+
+            switch (opcao) {
+                case 1:
+                    incluirSerie();
+                    break;
+                case 2:
+                    buscarSerie();
+                    break;
+                case 3:
+                    alterarSerie();
+                    break;
+                case 4:
+                    excluirSerie();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+    }
+
+    private void buscarSerie() {
+        String cpf = visao.lerCpf();
+        try {
+            Cliente cliente = arqClientes.read(cpf);
+            if (cliente != null) {
+                visao.mostraSerie(cliente);
+            } else {
+                System.out.println("Cliente não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar o cliente!");
+            e.printStackTrace();
+        }
+    }
+
+    private void incluirSerie() {
+        String nome = visao.obterNome();
+        if (nome.isEmpty()) return;
+
+        String cpf = visao.obterCpf();
+        float salario = visao.obterSalario();
+        LocalDate dataNascimento = visao.obterDataNascimento();
+
+        System.out.print("\nConfirma a inclusão do cliente? (S/N) ");
+        if (visao.confirmarAlteracoes()) {
+            try {
+                Cliente cliente = new Cliente(nome, cpf, salario, dataNascimento);
+                arqClientes.create(cliente);
+                System.out.println("Cliente incluído com sucesso.");
+            } catch (Exception e) {
+                System.out.println("Erro ao incluir o cliente!");
+            }
+        }
+    }
+
+    private void alterarSerie() throws Exception {
+        String cpf = visao.lerCpf();
+        Cliente cliente = arqClientes.read(cpf);
+        if (cliente != null) {
+            visao.mostraSerie(cliente);
+
+            String novoNome = visao.obterNome();
+            if (!novoNome.isEmpty()) cliente.nome = novoNome;
+
+            String novoCpf = visao.obterCpf();
+            if (!novoCpf.isEmpty()) cliente.cpf = novoCpf;
+
+            float novoSalario = visao.obterSalario();
+            cliente.salario = novoSalario;
+
+            LocalDate novaDataNascimento = visao.obterDataNascimento();
+            cliente.nascimento = novaDataNascimento;
+
+            if (visao.confirmarAlteracoes()) {
+                boolean alterado = arqClientes.update(cliente);
+                if (alterado) {
+                    System.out.println("Cliente alterado com sucesso.");
+                } else {
+                    System.out.println("Erro ao alterar o cliente.");
+                }
+            } else {
+                System.out.println("Alterações canceladas.");
+            }
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+
+    private void excluirSerie() throws Exception {
+        String cpf = visao.lerCpf();
+        Cliente cliente = arqClientes.read(cpf);
+        if (cliente != null) {
+            visao.mostraSerie(cliente);
+            if (visao.confirmarExclusao()) {
+                boolean excluido = arqClientes.delete(cpf);
+                if (excluido) {
+                    System.out.println("Cliente excluído com sucesso.");
+                } else {
+                    System.out.println("Erro ao excluir o cliente.");
+                }
+            } else {
+                System.out.println("Exclusão cancelada.");
+            }
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+}
---
+++ tp01/src/data/ArquivoEpisodio.java
@@ -0,0 +1,69 @@
+package tp01.src.data;
+
+import tp01.src.models.*;
+import tp01.src.util.*;
+import tp01.src.storeage.*;
+
+public class ArquivoEpisodio extends Arquivo<Episodio> {
+
+    Arquivo<Episodio> arqEpisodio;
+    HashExtensivel<ParCPFID> indiceIndiretoCPF;
+
+    public ArquivoEpisodio() throws Exception {
+        super("episodio", Episodio.class.getConstructor());
+
+        // Criação do índice indireto com HashExtensível, ajustando os caminhos para a
+        // pasta tp01/files
+        indiceIndiretoCPF = new HashExtensivel<>(
+                ParCPFID.class.getConstructor(),
+                4,
+                "tp01/files/episodio/indiceCPF.d.db", // Diretório para o índice de CPF
+                "tp01/files/episodio/indiceCPF.c.db" // Cestos para o índice de CPF
+        );
+    }
+
+    @Override
+    public int create(Episodio c) throws Exception {
+        int id = super.create(c);
+        indiceIndiretoCPF.create(new ParCPFID(c.getCpf(), id));
+        return id;
+    }
+
+    public Episodio read(String cpf) throws Exception {
+        ParCPFID pci = indiceIndiretoCPF.read(ParCPFID.hash(cpf));
+        if (pci == null)
+            return null;
+        return read(pci.getId());
+    }
+
+    public boolean delete(String cpf) throws Exception {
+        ParCPFID pci = indiceIndiretoCPF.read(ParCPFID.hash(cpf));
+        if (pci != null)
+            if (delete(pci.getId()))
+                return indiceIndiretoCPF.delete(ParCPFID.hash(cpf));
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) throws Exception {
+        Episodio c = super.read(id);
+        if (c != null) {
+            if (super.delete(id))
+                return indiceIndiretoCPF.delete(ParCPFID.hash(c.getCpf()));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Episodio novoEpisodio) throws Exception {
+        Episodio clienteVelho = read(novoEpisodio.getCpf());
+        if (super.update(novoEpisodio)) {
+            if (novoEpisodio.getCpf().compareTo(clienteVelho.getCpf()) != 0) {
+                indiceIndiretoCPF.delete(ParCPFID.hash(clienteVelho.getCpf()));
+                indiceIndiretoCPF.create(new ParCPFID(novoEpisodio.getCpf(), novoEpisodio.getId()));
+            }
+            return true;
+        }
+        return false;
+    }
+}
