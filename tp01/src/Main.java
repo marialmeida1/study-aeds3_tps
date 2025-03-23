6abbafdc2524667dca890812694e495b4ba27040
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-03-22 22:01:05 -0300

    style: organizando as pastas

---
+++ tp01/src/Main.java
@@ -0,0 +1,53 @@
+package tp01.src;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner console;
+
+        try {
+            console = new Scanner(System.in);
+            int opcao;
+            do {
+
+                System.out.println("\n\nPUCFlix 1.0");
+                System.out.println("-------");
+                System.out.println("> Início");
+                System.out.println("\n1 - Séries");
+                System.out.println("\n2 - Episódios");
+                System.out.println("0 - Sair");
+
+                System.out.print("\nOpção: ");
+                try {
+                    opcao = Integer.valueOf(console.nextLine());
+                } catch (NumberFormatException e) {
+                    opcao = -1;
+                }
+
+                switch (opcao) {
+                    case 1:
+                        /*(new MenuSeries()).menu();*/
+                        System.out.println("Menu Séries");
+                        break;
+                    case 2:
+                        /*(new MenuEpisodios()).menu();*/
+                        System.out.println("Menu Episódios");
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
+
+            } while (opcao != 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
